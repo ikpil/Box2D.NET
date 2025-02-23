5f82818135ff3c57322234f94a21e08267c73b26
ikpil <ikpil@naver.com>
2025-02-23 15:15:13 +0900

    BenchmarkCompound

--- src/Box2D.NET.Samples/Samples/Benchmarks/BenchmarkCompound.cs
+++ src/Box2D.NET.Samples/Samples/Benchmarks/BenchmarkCompound.cs
@@ -9,99 +9,96 @@ namespace Box2D.NET.Samples.Samples.Benchmarks;
 
 public class BenchmarkCompound : Sample
 {
-explicit BenchmarkCompound( Settings settings )
-    : Sample( settings )
-{
-    if ( settings.restart == false )
+    static int sampleCompound = RegisterSample("Benchmark", "Compound", Create);
+
+    static Sample Create(Settings settings)
     {
-        Draw.g_camera.m_center = { 18.0f, 115.0f };
-        Draw.g_camera.m_zoom = 25.0f * 5.5f;
+        return new BenchmarkCompound(settings);
     }
 
-    float grid = 1.0f;
-#ifdef NDEBUG
+    public BenchmarkCompound(Settings settings) : base(settings)
+    {
+        if (settings.restart == false)
+        {
+            Draw.g_camera.m_center = new b2Vec2(18.0f, 115.0f);
+            Draw.g_camera.m_zoom = 25.0f * 5.5f;
+        }
+
+        float grid = 1.0f;
+#if DEBUG
+        int height = 100;
+        int width = 100;
+#else
     int height = 200;
     int width = 200;
-#else
-    int height = 100;
-    int width = 100;
 #endif
-    {
-
-        b2BodyDef bodyDef = b2DefaultBodyDef();
-        b2BodyId groundId = b2CreateBody( m_worldId, &bodyDef );
-        b2ShapeDef shapeDef = b2DefaultShapeDef();
-
-        for ( int i = 0; i < height; ++i )
         {
-            float y = grid * i;
-            for ( int j = i; j < width; ++j )
+            b2BodyDef bodyDef = b2DefaultBodyDef();
+            b2BodyId groundId = b2CreateBody(m_worldId, bodyDef);
+            b2ShapeDef shapeDef = b2DefaultShapeDef();
+
+            for (int i = 0; i < height; ++i)
             {
-                float x = grid * j;
-                b2Polygon square = b2MakeOffsetBox( 0.5f * grid, 0.5f * grid, { x, y }, b2Rot_identity );
-                b2CreatePolygonShape( groundId, &shapeDef, &square );
+                float y = grid * i;
+                for (int j = i; j < width; ++j)
+                {
+                    float x = grid * j;
+                    b2Polygon square = b2MakeOffsetBox(0.5f * grid, 0.5f * grid, new b2Vec2(x, y), b2Rot_identity);
+                    b2CreatePolygonShape(groundId, shapeDef, square);
+                }
+            }
+
+            for (int i = 0; i < height; ++i)
+            {
+                float y = grid * i;
+                for (int j = i; j < width; ++j)
+                {
+                    float x = -grid * j;
+                    b2Polygon square = b2MakeOffsetBox(0.5f * grid, 0.5f * grid, new b2Vec2(x, y), b2Rot_identity);
+                    b2CreatePolygonShape(groundId, shapeDef, square);
+                }
             }
         }
 
-        for ( int i = 0; i < height; ++i )
         {
-            float y = grid * i;
-            for ( int j = i; j < width; ++j )
-            {
-                float x = -grid * j;
-                b2Polygon square = b2MakeOffsetBox( 0.5f * grid, 0.5f * grid, { x, y }, b2Rot_identity );
-                b2CreatePolygonShape( groundId, &shapeDef, &square );
-            }
-        }
-    }
-
-    {
-#ifdef NDEBUG
+#if DEBUG
+            int span = 5;
+            int count = 5;
+#else
         int span = 20;
         int count = 5;
-#else
-        int span = 5;
-        int count = 5;
 #endif
 
-        b2BodyDef bodyDef = b2DefaultBodyDef();
-        bodyDef.type = b2BodyType.b2_dynamicBody;
-        // defer mass properties to avoid n-squared mass computations
-        b2ShapeDef shapeDef = b2DefaultShapeDef();
-        shapeDef.updateBodyMass = false;
+            b2BodyDef bodyDef = b2DefaultBodyDef();
+            bodyDef.type = b2BodyType.b2_dynamicBody;
+            // defer mass properties to avoid n-squared mass computations
+            b2ShapeDef shapeDef = b2DefaultShapeDef();
+            shapeDef.updateBodyMass = false;
 
-        for ( int m = 0; m < count; ++m )
-        {
-            float ybody = ( 100.0f + m * span ) * grid;
-
-            for ( int n = 0; n < count; ++n )
+            for (int m = 0; m < count; ++m)
             {
-                float xbody = -0.5f * grid * count * span + n * span * grid;
-                bodyDef.position = { xbody, ybody };
-                b2BodyId bodyId = b2CreateBody( m_worldId, &bodyDef );
+                float ybody = (100.0f + m * span) * grid;
 
-                for ( int i = 0; i < span; ++i )
+                for (int n = 0; n < count; ++n)
                 {
-                    float y = i * grid;
-                    for ( int j = 0; j < span; ++j )
-                    {
-                        float x = j * grid;
-                        b2Polygon square = b2MakeOffsetBox( 0.5f * grid, 0.5f * grid, { x, y }, b2Rot_identity );
-                        b2CreatePolygonShape( bodyId, &shapeDef, &square );
-                    }
-                }
+                    float xbody = -0.5f * grid * count * span + n * span * grid;
+                    bodyDef.position = new b2Vec2(xbody, ybody);
+                    b2BodyId bodyId = b2CreateBody(m_worldId, bodyDef);
 
-                // All shapes have been added so I can efficiently compute the mass properties.
-                b2Body_ApplyMassFromShapes( bodyId );
+                    for (int i = 0; i < span; ++i)
+                    {
+                        float y = i * grid;
+                        for (int j = 0; j < span; ++j)
+                        {
+                            float x = j * grid;
+                            b2Polygon square = b2MakeOffsetBox(0.5f * grid, 0.5f * grid, new b2Vec2(x, y), b2Rot_identity);
+                            b2CreatePolygonShape(bodyId, shapeDef, square);
+                        }
+                    }
+
+                    // All shapes have been added so I can efficiently compute the mass properties.
+                    b2Body_ApplyMassFromShapes(bodyId);
+                }
             }
         }
     }
-}
-
-static Sample* Create( Settings settings )
-{
-    return new BenchmarkCompound( settings );
-}
-};
-
-static int sampleCompound = RegisterSample( "Benchmark", "Compound", BenchmarkCompound::Create );
