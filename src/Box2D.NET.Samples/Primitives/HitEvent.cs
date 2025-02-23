addf4b1de2aabfdf9e095ac963d737116e9c9557
ikpil <ikpil@naver.com>
2025-02-23 20:25:59 +0900

    BounceHouse

---
+++ src/Box2D.NET.Samples/Primitives/HitEvent.cs
@@ -0,0 +1,17 @@
+﻿using Box2D.NET.Primitives;
+
+namespace Box2D.NET.Samples.Primitives;
+
+public struct HitEvent
+{
+    public b2Vec2 point;
+    public float speed;
+    public int stepIndex;
+
+    public void Clear()
+    {
+        point = new b2Vec2();
+        speed = 0.0f;
+        stepIndex = 0;
+    }
+}
--- src/Box2D.NET.Samples/Samples/Continuous/BounceHouse.cs
+++ src/Box2D.NET.Samples/Samples/Continuous/BounceHouse.cs
@@ -1,4 +1,6 @@
-﻿using Box2D.NET.Primitives;
+﻿using System.Numerics;
+using Box2D.NET.Primitives;
+using Box2D.NET.Samples.Primitives;
 using ImGuiNET;
 using static Box2D.NET.id;
 using static Box2D.NET.geometry;
@@ -9,179 +11,175 @@ using static Box2D.NET.world;
 
 namespace Box2D.NET.Samples.Samples.Continuous;
 
-class BounceHouse : Sample
+public class BounceHouse : Sample
 {
-public:
-enum ShapeType
-{
-    e_circleShape = 0,
-    e_capsuleShape,
-    e_boxShape
-};
-
-struct HitEvent
-{
-    b2Vec2 point;
-    float speed;
-    int stepIndex;
-};
-
-public BounceHouse( Settings settings )
-    : base( settings )
-{
-    if ( settings.restart == false )
+    public enum ShapeType
     {
-        Draw.g_camera.m_center = { 0.0f, 0.0f };
-        Draw.g_camera.m_zoom = 25.0f * 0.45f;
+        e_circleShape = 0,
+        e_capsuleShape,
+        e_boxShape
+    };
+
+
+    HitEvent[] m_hitEvents = new HitEvent[4];
+    b2BodyId m_bodyId;
+    ShapeType m_shapeType;
+    bool m_enableHitEvents;
+    static int sampleBounceHouse = RegisterSample("Continuous", "Bounce House", Create);
+
+    static Sample Create(Settings settings)
+    {
+        return new BounceHouse(settings);
     }
 
-    b2BodyDef bodyDef = b2DefaultBodyDef();
-    b2BodyId groundId = b2CreateBody( m_worldId, &bodyDef );
-
-    b2ShapeDef shapeDef = b2DefaultShapeDef();
+    public BounceHouse(Settings settings) : base(settings)
     {
-        b2Segment segment = { { -10.0f, -10.0f }, { 10.0f, -10.0f } };
-        b2CreateSegmentShape( groundId, &shapeDef, &segment );
-    }
+        if (settings.restart == false)
+        {
+            Draw.g_camera.m_center = new b2Vec2(0.0f, 0.0f);
+            Draw.g_camera.m_zoom = 25.0f * 0.45f;
+        }
 
-    {
-        b2Segment segment = { { 10.0f, -10.0f }, { 10.0f, 10.0f } };
-        b2CreateSegmentShape( groundId, &shapeDef, &segment );
-    }
+        b2BodyDef bodyDef = b2DefaultBodyDef();
+        b2BodyId groundId = b2CreateBody(m_worldId, bodyDef);
 
-    {
-        b2Segment segment = { { 10.0f, 10.0f }, { -10.0f, 10.0f } };
-        b2CreateSegmentShape( groundId, &shapeDef, &segment );
-    }
+        b2ShapeDef shapeDef = b2DefaultShapeDef();
+        {
+            b2Segment segment = new b2Segment(new b2Vec2(-10.0f, -10.0f), new b2Vec2(10.0f, -10.0f));
+            b2CreateSegmentShape(groundId, shapeDef, segment);
+        }
 
-    {
-        b2Segment segment = { { -10.0f, 10.0f }, { -10.0f, -10.0f } };
-        b2CreateSegmentShape( groundId, &shapeDef, &segment );
-    }
+        {
+            b2Segment segment = new b2Segment(new b2Vec2(10.0f, -10.0f), new b2Vec2(10.0f, 10.0f));
+            b2CreateSegmentShape(groundId, shapeDef, segment);
+        }
 
-    m_shapeType = e_boxShape;
-    m_bodyId = b2_nullBodyId;
-    m_enableHitEvents = true;
+        {
+            b2Segment segment = new b2Segment(new b2Vec2(10.0f, 10.0f), new b2Vec2(-10.0f, 10.0f));
+            b2CreateSegmentShape(groundId, shapeDef, segment);
+        }
 
-    memset( m_hitEvents, 0, sizeof( m_hitEvents ) );
+        {
+            b2Segment segment = new b2Segment(new b2Vec2(-10.0f, 10.0f), new b2Vec2(-10.0f, -10.0f));
+            b2CreateSegmentShape(groundId, shapeDef, segment);
+        }
 
-    Launch();
-}
+        m_shapeType = ShapeType.e_boxShape;
+        m_bodyId = b2_nullBodyId;
+        m_enableHitEvents = true;
 
-void Launch()
-{
-    if ( B2_IS_NON_NULL( m_bodyId ) )
-    {
-        b2DestroyBody( m_bodyId );
-    }
+        //memset( m_hitEvents, 0, sizeof( m_hitEvents ) );
+        for (int i = 0; i < m_hitEvents.Length; ++i)
+        {
+            m_hitEvents[i].Clear();
+        }
 
-    b2BodyDef bodyDef = b2DefaultBodyDef();
-    bodyDef.type = b2BodyType.b2_dynamicBody;
-    bodyDef.linearVelocity = { 10.0f, 20.0f };
-    bodyDef.position = { 0.0f, 0.0f };
-    bodyDef.gravityScale = 0.0f;
-
-    // Circle shapes centered on the body can spin fast without risk of tunnelling.
-    bodyDef.allowFastRotation = m_shapeType == e_circleShape;
-
-    m_bodyId = b2CreateBody( m_worldId, &bodyDef );
-
-    b2ShapeDef shapeDef = b2DefaultShapeDef();
-    shapeDef.density = 1.0f;
-    shapeDef.restitution = 1.2f;
-    shapeDef.friction = 0.3f;
-    shapeDef.enableHitEvents = m_enableHitEvents;
-
-    if ( m_shapeType == e_circleShape )
-    {
-        b2Circle circle = { { 0.0f, 0.0f }, 0.5f };
-        b2CreateCircleShape( m_bodyId, &shapeDef, &circle );
-    }
-    else if ( m_shapeType == e_capsuleShape )
-    {
-        b2Capsule capsule = { { -0.5f, 0.0f }, { 0.5f, 0.0 }, 0.25f };
-        b2CreateCapsuleShape( m_bodyId, &shapeDef, &capsule );
-    }
-    else
-    {
-        float h = 0.1f;
-        b2Polygon box = b2MakeBox( 20.0f * h, h );
-        b2CreatePolygonShape( m_bodyId, &shapeDef, &box );
-    }
-}
-
-public override void UpdateUI()
-{
-    float height = 100.0f;
-    ImGui.SetNextWindowPos( new Vector2( 10.0f, Draw.g_camera.m_height - height - 50.0f ), ImGuiCond.Once );
-    ImGui.SetNextWindowSize( new Vector2( 240.0f, height ) );
-
-    ImGui.Begin( "Bounce House", nullptr, ImGuiWindowFlags.NoResize );
-
-    const char* shapeTypes[] = { "Circle", "Capsule", "Box" };
-    int shapeType = int( m_shapeType );
-    if ( ImGui.Combo( "Shape", &shapeType, shapeTypes, IM_ARRAYSIZE( shapeTypes ) ) )
-    {
-        m_shapeType = ShapeType( shapeType );
         Launch();
     }
 
-    if ( ImGui.Checkbox( "hit events", &m_enableHitEvents ) )
+    void Launch()
     {
-        b2Body_EnableHitEvents( m_bodyId, m_enableHitEvents );
+        if (B2_IS_NON_NULL(m_bodyId))
+        {
+            b2DestroyBody(m_bodyId);
+        }
+
+        b2BodyDef bodyDef = b2DefaultBodyDef();
+        bodyDef.type = b2BodyType.b2_dynamicBody;
+        bodyDef.linearVelocity = new b2Vec2(10.0f, 20.0f);
+        bodyDef.position = new b2Vec2(0.0f, 0.0f);
+        bodyDef.gravityScale = 0.0f;
+
+        // Circle shapes centered on the body can spin fast without risk of tunnelling.
+        bodyDef.allowFastRotation = m_shapeType == ShapeType.e_circleShape;
+
+        m_bodyId = b2CreateBody(m_worldId, bodyDef);
+
+        b2ShapeDef shapeDef = b2DefaultShapeDef();
+        shapeDef.density = 1.0f;
+        shapeDef.restitution = 1.2f;
+        shapeDef.friction = 0.3f;
+        shapeDef.enableHitEvents = m_enableHitEvents;
+
+        if (m_shapeType == ShapeType.e_circleShape)
+        {
+            b2Circle circle = new b2Circle(new b2Vec2(0.0f, 0.0f), 0.5f);
+            b2CreateCircleShape(m_bodyId, shapeDef, circle);
+        }
+        else if (m_shapeType == ShapeType.e_capsuleShape)
+        {
+            b2Capsule capsule = new b2Capsule(new b2Vec2(-0.5f, 0.0f), new b2Vec2(0.5f, 0.0f), 0.25f);
+            b2CreateCapsuleShape(m_bodyId, shapeDef, capsule);
+        }
+        else
+        {
+            float h = 0.1f;
+            b2Polygon box = b2MakeBox(20.0f * h, h);
+            b2CreatePolygonShape(m_bodyId, shapeDef, box);
+        }
     }
 
-    ImGui.End();
-}
-
-public override void Step(Settings settings)
-{
-    base.Step( settings );
-
-    b2ContactEvents events = b2World_GetContactEvents( m_worldId );
-    for ( int i = 0; i < events.hitCount; ++i )
+    public override void UpdateUI()
     {
-        b2ContactHitEvent* event = events.hitEvents + i;
+        bool open = false;
+        float height = 100.0f;
+        ImGui.SetNextWindowPos(new Vector2(10.0f, Draw.g_camera.m_height - height - 50.0f), ImGuiCond.Once);
+        ImGui.SetNextWindowSize(new Vector2(240.0f, height));
 
-        HitEvent* e = m_hitEvents + 0;
-        for ( int j = 1; j < 4; ++j )
+        ImGui.Begin("Bounce House", ref open, ImGuiWindowFlags.NoResize);
+
+        string[] shapeTypes = { "Circle", "Capsule", "Box" };
+        int shapeType = (int)m_shapeType;
+        if (ImGui.Combo("Shape", ref shapeType, shapeTypes, shapeTypes.Length))
         {
-            if ( m_hitEvents[j].stepIndex < e->stepIndex )
+            m_shapeType = (ShapeType)shapeType;
+            Launch();
+        }
+
+        if (ImGui.Checkbox("hit events", ref m_enableHitEvents))
+        {
+            b2Body_EnableHitEvents(m_bodyId, m_enableHitEvents);
+        }
+
+        ImGui.End();
+    }
+
+    public override void Step(Settings settings)
+    {
+        base.Step(settings);
+
+        b2ContactEvents events = b2World_GetContactEvents(m_worldId);
+        for (int i = 0; i < events.hitCount; ++i)
+        {
+            b2ContactHitEvent @event = events.hitEvents[i];
+
+            ref HitEvent e = ref m_hitEvents[0];
+            for (int j = 1; j < 4; ++j)
             {
-                e = m_hitEvents + j;
+                if (m_hitEvents[j].stepIndex < e.stepIndex)
+                {
+                    e = ref m_hitEvents[j];
+                }
+            }
+
+            e.point = @event.point;
+            e.speed = @event.approachSpeed;
+            e.stepIndex = m_stepCount;
+        }
+
+        for (int i = 0; i < 4; ++i)
+        {
+            ref HitEvent e = ref m_hitEvents[i];
+            if (e.stepIndex > 0 && m_stepCount <= e.stepIndex + 30)
+            {
+                Draw.g_draw.DrawCircle(e.point, 0.1f, b2HexColor.b2_colorOrangeRed);
+                Draw.g_draw.DrawString(e.point, "%.1f", e.speed);
             }
         }
 
-        e->point = event->point;
-        e->speed = event->approachSpeed;
-        e->stepIndex = m_stepCount;
-    }
-
-    for ( int i = 0; i < 4; ++i )
-    {
-        HitEvent* e = m_hitEvents + i;
-        if ( e->stepIndex > 0 && m_stepCount <= e->stepIndex + 30 )
+        if (m_stepCount == 1000)
         {
-            Draw.g_draw.DrawCircle( e->point, 0.1f, b2HexColor.b2_colorOrangeRed );
-            Draw.g_draw.DrawString( e->point, "%.1f", e->speed );
+            m_stepCount += 0;
         }
     }
-
-    if ( m_stepCount == 1000 )
-    {
-        m_stepCount += 0;
-    }
 }
-
-static Sample Create( Settings settings )
-{
-    return new BounceHouse( settings );
-}
-
-HitEvent m_hitEvents[4];
-b2BodyId m_bodyId;
-ShapeType m_shapeType;
-bool m_enableHitEvents;
-};
-
-static int sampleBounceHouse = RegisterSample( "Continuous", "Bounce House", BounceHouse::Create );
