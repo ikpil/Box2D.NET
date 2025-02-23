52512b1f671962a9b32192e1983e98481f86c07b
ikpil <ikpil@naver.com>
2025-02-23 15:19:21 +0900

    BenchmarkJointGrid

--- src/Box2D.NET.Samples/Samples/Benchmarks/BenchmarkJointGrid.cs
+++ src/Box2D.NET.Samples/Samples/Benchmarks/BenchmarkJointGrid.cs
@@ -1,23 +1,26 @@
-﻿namespace Box2D.NET.Samples.Samples.Benchmarks;
+﻿using Box2D.NET.Primitives;
+using static Box2D.NET.Shared.benchmarks;
+
+namespace Box2D.NET.Samples.Samples.Benchmarks;
 
 public class BenchmarkJointGrid : Sample
 {
-    BenchmarkJointGrid( Settings settings ) : base( settings )
+    static int benchmarkJointGridIndex = RegisterSample("Benchmark", "Joint Grid", Create);
+
+    static Sample Create(Settings settings)
     {
-        if ( settings.restart == false )
+        return new BenchmarkJointGrid(settings);
+    }
+
+    public BenchmarkJointGrid(Settings settings) : base(settings)
+    {
+        if (settings.restart == false)
         {
-            Draw.g_camera.m_center = { 60.0f, -57.0f };
+            Draw.g_camera.m_center = new b2Vec2(60.0f, -57.0f);
             Draw.g_camera.m_zoom = 25.0f * 2.5f;
             settings.enableSleep = false;
         }
 
-        CreateJointGrid( m_worldId );
+        CreateJointGrid(m_worldId);
     }
-
-    static Sample* Create( Settings settings )
-    {
-        return new BenchmarkJointGrid( settings );
-    }
-};
-
-static int benchmarkJointGridIndex = RegisterSample( "Benchmark", "Joint Grid", BenchmarkJointGrid::Create );
+}
