634c61eee7d63781c9e184fa89db4fd57d404947
ikpil <ikpil@naver.com>
2025-02-24 23:25:44 +0900

    CardHouse

--- src/Box2D.NET.Samples/Samples/Stackings/CardHouse.cs
+++ src/Box2D.NET.Samples/Samples/Stackings/CardHouse.cs
@@ -10,30 +10,31 @@ namespace Box2D.NET.Samples.Samples.Stackings;
 // From PEEL
 public class CardHouse : Sample
 {
-    static int sampleCardHouse = RegisterSample( "Stacking", "Card House", Create );
-    static Sample Create( Settings settings )
+    static int sampleCardHouse = RegisterSample("Stacking", "Card House", Create);
+
+    static Sample Create(Settings settings)
     {
-        return new CardHouse( settings );
+        return new CardHouse(settings);
     }
 
-    public CardHouse( Settings settings )
-        : base( settings )
+    public CardHouse(Settings settings)
+        : base(settings)
     {
-        if ( settings.restart == false )
+        if (settings.restart == false)
         {
-            Draw.g_camera.m_center = { 0.75f, 0.9f };
+            Draw.g_camera.m_center = new b2Vec2(0.75f, 0.9f);
             Draw.g_camera.m_zoom = 25.0f * 0.05f;
         }
 
         b2BodyDef bodyDef = b2DefaultBodyDef();
-        bodyDef.position = { 0.0f, -2.0f };
-        b2BodyId groundId = b2CreateBody( m_worldId, &bodyDef );
+        bodyDef.position = new b2Vec2(0.0f, -2.0f);
+        b2BodyId groundId = b2CreateBody(m_worldId, bodyDef);
 
         b2ShapeDef shapeDef = b2DefaultShapeDef();
         shapeDef.friction = 0.7f;
 
-        b2Polygon groundBox = b2MakeBox( 40.0f, 2.0f );
-        b2CreatePolygonShape( groundId, &shapeDef, &groundBox );
+        b2Polygon groundBox = b2MakeBox(40.0f, 2.0f);
+        b2CreatePolygonShape(groundId, shapeDef, groundBox);
 
         float cardHeight = 0.2f;
         float cardThickness = 0.001f;
@@ -42,43 +43,45 @@ public class CardHouse : Sample
         float angle1 = -25.0f * B2_PI / 180.0f;
         float angle2 = 0.5f * B2_PI;
 
-        b2Polygon cardBox = b2MakeBox( cardThickness, cardHeight );
+        b2Polygon cardBox = b2MakeBox(cardThickness, cardHeight);
         bodyDef.type = b2BodyType.b2_dynamicBody;
 
         int Nb = 5;
         float z0 = 0.0f;
         float y = cardHeight - 0.02f;
-        while ( Nb )
+        while (0 < Nb)
         {
             float z = z0;
-            for ( int i = 0; i < Nb; i++ )
+            for (int i = 0; i < Nb; i++)
             {
-                if ( i != Nb - 1 )
+                if (i != Nb - 1)
                 {
-                    bodyDef.position = { z + 0.25f, y + cardHeight - 0.015f };
-                    bodyDef.rotation = b2MakeRot( angle2 );
-                    b2BodyId bodyId = b2CreateBody( m_worldId, &bodyDef );
-                    b2CreatePolygonShape( bodyId, &shapeDef, &cardBox );
+                    bodyDef.position = new b2Vec2(z + 0.25f, y + cardHeight - 0.015f);
+                    bodyDef.rotation = b2MakeRot(angle2);
+                    b2BodyId bodyId = b2CreateBody(m_worldId, bodyDef);
+                    b2CreatePolygonShape(bodyId, shapeDef, cardBox);
                 }
 
-                bodyDef.position = { z, y };
-                bodyDef.rotation = b2MakeRot( angle1 );
-                b2BodyId bodyId = b2CreateBody( m_worldId, &bodyDef );
-                b2CreatePolygonShape( bodyId, &shapeDef, &cardBox );
+                {
+                    bodyDef.position = new b2Vec2(z, y);
+                    bodyDef.rotation = b2MakeRot(angle1);
+                    b2BodyId bodyId = b2CreateBody(m_worldId, bodyDef);
+                    b2CreatePolygonShape(bodyId, shapeDef, cardBox);
 
-                z += 0.175f;
+                    z += 0.175f;
 
-                bodyDef.position = { z, y };
-                bodyDef.rotation = b2MakeRot( angle0 );
-                bodyId = b2CreateBody( m_worldId, &bodyDef );
-                b2CreatePolygonShape( bodyId, &shapeDef, &cardBox );
+                    bodyDef.position = new b2Vec2(z, y);
+                    bodyDef.rotation = b2MakeRot(angle0);
+                    bodyId = b2CreateBody(m_worldId, bodyDef);
+                    b2CreatePolygonShape(bodyId, shapeDef, cardBox);
 
-                z += 0.175f;
+                    z += 0.175f;
+                }
             }
+
             y += cardHeight * 2.0f - 0.03f;
             z0 += 0.175f;
             Nb--;
         }
     }
-
 }
