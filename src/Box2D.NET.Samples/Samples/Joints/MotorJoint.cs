eb603aee83051eea3a0f064f34ec357d47e68bf6
ikpil <ikpil@naver.com>
2025-02-24 13:36:40 +0900

    MotorJoint

--- src/Box2D.NET.Samples/Samples/Joints/MotorJoint.cs
+++ src/Box2D.NET.Samples/Samples/Joints/MotorJoint.cs
@@ -26,120 +26,117 @@ public class MotorJoint : Sample
     float m_correctionFactor;
     bool m_go;
 
-    static int sampleMotorJoint = RegisterSample( "Joints", "Motor Joint", Create );
-    static Sample Create( Settings settings )
+    static int sampleMotorJoint = RegisterSample("Joints", "Motor Joint", Create);
+
+    static Sample Create(Settings settings)
     {
-        return new MotorJoint( settings );
+        return new MotorJoint(settings);
     }
 
-public MotorJoint( Settings settings )
-    : base( settings )
-{
-    if ( settings.restart == false )
+    public MotorJoint(Settings settings)
+        : base(settings)
     {
-        Draw.g_camera.m_center = { 0.0f, 7.0f };
-        Draw.g_camera.m_zoom = 25.0f * 0.4f;
+        if (settings.restart == false)
+        {
+            Draw.g_camera.m_center = new b2Vec2(0.0f, 7.0f);
+            Draw.g_camera.m_zoom = 25.0f * 0.4f;
+        }
+
+        b2BodyId groundId;
+        {
+            b2BodyDef bodyDef = b2DefaultBodyDef();
+            groundId = b2CreateBody(m_worldId, bodyDef);
+            b2ShapeDef shapeDef = b2DefaultShapeDef();
+            b2Segment segment = new b2Segment(new b2Vec2(-20.0f, 0.0f), new b2Vec2(20.0f, 0.0f));
+            b2CreateSegmentShape(groundId, shapeDef, segment);
+        }
+
+        // Define motorized body
+        {
+            b2BodyDef bodyDef = b2DefaultBodyDef();
+            bodyDef.type = b2BodyType.b2_dynamicBody;
+            bodyDef.position = new b2Vec2(0.0f, 8.0f);
+            b2BodyId bodyId = b2CreateBody(m_worldId, bodyDef);
+
+            b2Polygon box = b2MakeBox(2.0f, 0.5f);
+            b2ShapeDef shapeDef = b2DefaultShapeDef();
+            shapeDef.density = 1.0f;
+            b2CreatePolygonShape(bodyId, shapeDef, box);
+
+            m_maxForce = 500.0f;
+            m_maxTorque = 500.0f;
+            m_correctionFactor = 0.3f;
+
+            b2MotorJointDef jointDef = b2DefaultMotorJointDef();
+            jointDef.bodyIdA = groundId;
+            jointDef.bodyIdB = bodyId;
+            jointDef.maxForce = m_maxForce;
+            jointDef.maxTorque = m_maxTorque;
+            jointDef.correctionFactor = m_correctionFactor;
+
+            m_jointId = b2CreateMotorJoint(m_worldId, jointDef);
+        }
+
+        m_go = true;
+        m_time = 0.0f;
     }
 
-    b2BodyId groundId;
+    public override void UpdateUI()
     {
-        b2BodyDef bodyDef = b2DefaultBodyDef();
-        groundId = b2CreateBody( m_worldId, &bodyDef );
-        b2ShapeDef shapeDef = b2DefaultShapeDef();
-        b2Segment segment = { { -20.0f, 0.0f }, { 20.0f, 0.0f } };
-        b2CreateSegmentShape( groundId, &shapeDef, &segment );
+        bool open = false;
+        float height = 140.0f;
+        ImGui.SetNextWindowPos(new Vector2(10.0f, Draw.g_camera.m_height - height - 50.0f), ImGuiCond.Once);
+        ImGui.SetNextWindowSize(new Vector2(240.0f, height));
+
+        ImGui.Begin("Motor Joint", ref open, ImGuiWindowFlags.NoResize);
+
+        if (ImGui.Checkbox("Go", ref m_go))
+        {
+        }
+
+        if (ImGui.SliderFloat("Max Force", ref m_maxForce, 0.0f, 1000.0f, "%.0f"))
+        {
+            b2MotorJoint_SetMaxForce(m_jointId, m_maxForce);
+        }
+
+        if (ImGui.SliderFloat("Max Torque", ref m_maxTorque, 0.0f, 1000.0f, "%.0f"))
+        {
+            b2MotorJoint_SetMaxTorque(m_jointId, m_maxTorque);
+        }
+
+        if (ImGui.SliderFloat("Correction", ref m_correctionFactor, 0.0f, 1.0f, "%.1f"))
+        {
+            b2MotorJoint_SetCorrectionFactor(m_jointId, m_correctionFactor);
+        }
+
+        ImGui.End();
     }
 
-    // Define motorized body
+    public override void Step(Settings settings)
     {
-        b2BodyDef bodyDef = b2DefaultBodyDef();
-        bodyDef.type = b2BodyType.b2_dynamicBody;
-        bodyDef.position = { 0.0f, 8.0f };
-        b2BodyId bodyId = b2CreateBody( m_worldId, &bodyDef );
+        if (m_go && settings.hertz > 0.0f)
+        {
+            m_time += 1.0f / settings.hertz;
+        }
 
-        b2Polygon box = b2MakeBox( 2.0f, 0.5f );
-        b2ShapeDef shapeDef = b2DefaultShapeDef();
-        shapeDef.density = 1.0f;
-        b2CreatePolygonShape( bodyId, &shapeDef, &box );
+        b2Vec2 linearOffset;
+        linearOffset.x = 6.0f * MathF.Sin(2.0f * m_time);
+        linearOffset.y = 8.0f + 4.0f * MathF.Sin(1.0f * m_time);
 
-        m_maxForce = 500.0f;
-        m_maxTorque = 500.0f;
-        m_correctionFactor = 0.3f;
+        float angularOffset = B2_PI * MathF.Sin(-0.5f * m_time);
 
-        b2MotorJointDef jointDef = b2DefaultMotorJointDef();
-        jointDef.bodyIdA = groundId;
-        jointDef.bodyIdB = bodyId;
-        jointDef.maxForce = m_maxForce;
-        jointDef.maxTorque = m_maxTorque;
-        jointDef.correctionFactor = m_correctionFactor;
+        b2MotorJoint_SetLinearOffset(m_jointId, linearOffset);
+        b2MotorJoint_SetAngularOffset(m_jointId, angularOffset);
 
-        m_jointId = b2CreateMotorJoint( m_worldId, &jointDef );
+        b2Transform transform = new b2Transform(linearOffset, b2MakeRot(angularOffset));
+        Draw.g_draw.DrawTransform(transform);
+
+        base.Step(settings);
+
+        b2Vec2 force = b2Joint_GetConstraintForce(m_jointId);
+        float torque = b2Joint_GetConstraintTorque(m_jointId);
+
+        Draw.g_draw.DrawString(5, m_textLine, "force = {%3.f, %3.f}, torque = %3.f", force.x, force.y, torque);
+        m_textLine += 15;
     }
-
-    m_go = true;
-    m_time = 0.0f;
 }
-
-public override void UpdateUI()
-{
-    float height = 140.0f;
-    ImGui.SetNextWindowPos( new Vector2( 10.0f, Draw.g_camera.m_height - height - 50.0f ), ImGuiCond.Once );
-    ImGui.SetNextWindowSize( new Vector2( 240.0f, height ) );
-
-    ImGui.Begin( "Motor Joint", nullptr, ImGuiWindowFlags.NoResize );
-
-    if ( ImGui.Checkbox( "Go", &m_go ) )
-    {
-    }
-
-    if ( ImGui.SliderFloat( "Max Force", &m_maxForce, 0.0f, 1000.0f, "%.0f" ) )
-    {
-        b2MotorJoint_SetMaxForce( m_jointId, m_maxForce );
-    }
-
-    if ( ImGui.SliderFloat( "Max Torque", &m_maxTorque, 0.0f, 1000.0f, "%.0f" ) )
-    {
-        b2MotorJoint_SetMaxTorque( m_jointId, m_maxTorque );
-    }
-
-    if ( ImGui.SliderFloat( "Correction", &m_correctionFactor, 0.0f, 1.0f, "%.1f" ) )
-    {
-        b2MotorJoint_SetCorrectionFactor( m_jointId, m_correctionFactor );
-    }
-
-    ImGui.End();
-}
-
-public override void Step(Settings settings)
-{
-    if ( m_go && settings.hertz > 0.0f )
-    {
-        m_time += 1.0f / settings.hertz;
-    }
-
-    b2Vec2 linearOffset;
-    linearOffset.x = 6.0f * MathF.Sin( 2.0f * m_time );
-    linearOffset.y = 8.0f + 4.0f * MathF.Sin( 1.0f * m_time );
-
-    float angularOffset = B2_PI * MathF.Sin( -0.5f * m_time );
-
-    b2MotorJoint_SetLinearOffset( m_jointId, linearOffset );
-    b2MotorJoint_SetAngularOffset( m_jointId, angularOffset );
-
-    b2Transform transform = { linearOffset, b2MakeRot( angularOffset ) };
-    Draw.g_draw.DrawTransform( transform );
-
-    base.Step( settings );
-
-    b2Vec2 force = b2Joint_GetConstraintForce( m_jointId );
-    float torque = b2Joint_GetConstraintTorque( m_jointId );
-
-    Draw.g_draw.DrawString( 5, m_textLine, "force = {%3.f, %3.f}, torque = %3.f", force.x, force.y, torque );
-    m_textLine += 15;
-}
-
-
-
-}
-
-
