6fafcd0a1f1347d54a7e7c09cdb2788947476777
ikpil <ikpil@naver.com>
2025-02-24 13:53:43 +0900

    HighMassRatio1

--- src/Box2D.NET.Samples/Samples/Robustness/HighMassRatio1.cs
+++ src/Box2D.NET.Samples/Samples/Robustness/HighMassRatio1.cs
@@ -10,62 +10,60 @@ namespace Box2D.NET.Samples.Samples.Robustness;
 // Pyramid with heavy box on top
 public class HighMassRatio1 : Sample
 {
-    static Sample Create( Settings settings )
+    static Sample Create(Settings settings)
     {
-        return new HighMassRatio1( settings );
-    }
-    static int sampleIndex1 = RegisterSample( "Robustness", "HighMassRatio1", Create );
-
-public HighMassRatio1( Settings settings )
-    : base( settings )
-{
-    if ( settings.restart == false )
-    {
-        Draw.g_camera.m_center = { 3.0f, 14.0f };
-        Draw.g_camera.m_zoom = 25.0f;
+        return new HighMassRatio1(settings);
     }
 
-    float extent = 1.0f;
+    static int sampleIndex1 = RegisterSample("Robustness", "HighMassRatio1", Create);
 
+    public HighMassRatio1(Settings settings) : base(settings)
     {
-        b2BodyDef bodyDef = b2DefaultBodyDef();
-        b2BodyId groundId = b2CreateBody( m_worldId, &bodyDef );
-        b2ShapeDef shapeDef = b2DefaultShapeDef();
-        b2Polygon box = b2MakeOffsetBox( 50.0f, 1.0f, { 0.0f, -1.0f }, b2Rot_identity );
-        b2CreatePolygonShape( groundId, &shapeDef, &box );
-    }
-
-    {
-        b2BodyDef bodyDef = b2DefaultBodyDef();
-        bodyDef.type = b2BodyType.b2_dynamicBody;
-        b2Polygon box = b2MakeBox( extent, extent );
-        b2ShapeDef shapeDef = b2DefaultShapeDef();
-
-        for ( int j = 0; j < 3; ++j )
+        if (settings.restart == false)
         {
-            int count = 10;
-            float offset = -20.0f * extent + 2.0f * ( count + 1.0f ) * extent * j;
-            float y = extent;
-            while ( count > 0 )
+            Draw.g_camera.m_center = new b2Vec2(3.0f, 14.0f);
+            Draw.g_camera.m_zoom = 25.0f;
+        }
+
+        float extent = 1.0f;
+
+        {
+            b2BodyDef bodyDef = b2DefaultBodyDef();
+            b2BodyId groundId = b2CreateBody(m_worldId, bodyDef);
+            b2ShapeDef shapeDef = b2DefaultShapeDef();
+            b2Polygon box = b2MakeOffsetBox(50.0f, 1.0f, new b2Vec2(0.0f, -1.0f), b2Rot_identity);
+            b2CreatePolygonShape(groundId, shapeDef, box);
+        }
+
+        {
+            b2BodyDef bodyDef = b2DefaultBodyDef();
+            bodyDef.type = b2BodyType.b2_dynamicBody;
+            b2Polygon box = b2MakeBox(extent, extent);
+            b2ShapeDef shapeDef = b2DefaultShapeDef();
+
+            for (int j = 0; j < 3; ++j)
             {
-                for ( int i = 0; i < count; ++i )
+                int count = 10;
+                float offset = -20.0f * extent + 2.0f * (count + 1.0f) * extent * j;
+                float y = extent;
+                while (count > 0)
                 {
-                    float coeff = i - 0.5f * count;
+                    for (int i = 0; i < count; ++i)
+                    {
+                        float coeff = i - 0.5f * count;
 
-                    float yy = count == 1 ? y + 2.0f : y;
-                    bodyDef.position = { 2.0f * coeff * extent + offset, yy };
-                    b2BodyId bodyId = b2CreateBody( m_worldId, &bodyDef );
+                        float yy = count == 1 ? y + 2.0f : y;
+                        bodyDef.position = new b2Vec2(2.0f * coeff * extent + offset, yy);
+                        b2BodyId bodyId = b2CreateBody(m_worldId, bodyDef);
 
-                    shapeDef.density = count == 1 ? ( j + 1.0f ) * 100.0f : 1.0f;
-                    b2CreatePolygonShape( bodyId, &shapeDef, &box );
+                        shapeDef.density = count == 1 ? (j + 1.0f) * 100.0f : 1.0f;
+                        b2CreatePolygonShape(bodyId, shapeDef, box);
+                    }
+
+                    --count;
+                    y += 2.0f * extent;
                 }
-
-                --count;
-                y += 2.0f * extent;
             }
         }
     }
 }
-
-}
-
