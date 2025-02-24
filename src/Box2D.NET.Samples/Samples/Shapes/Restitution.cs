cf18450f6919eaef2cf24b03dbd511496989451b
ikpil <ikpil@naver.com>
2025-02-24 22:59:24 +0900

    Restitution

--- src/Box2D.NET.Samples/Samples/Shapes/Restitution.cs
+++ src/Box2D.NET.Samples/Samples/Shapes/Restitution.cs
@@ -1,5 +1,5 @@
-﻿using Box2D.NET.Primitives;
-using Box2D.NET.Samples;
+﻿using System.Numerics;
+using Box2D.NET.Primitives;
 using ImGuiNET;
 using static Box2D.NET.id;
 using static Box2D.NET.geometry;
@@ -21,60 +21,61 @@ public class Restitution : Sample
     public const int e_count = 40;
 
 
-    b2BodyId m_bodyIds[e_count];
+    b2BodyId[] m_bodyIds = new b2BodyId[e_count];
     ShapeType m_shapeType;
 
-    static int sampleIndex = RegisterSample( "Shapes", "Restitution", Create );
-    static Sample Create( Settings settings )
+    static int sampleIndex = RegisterSample("Shapes", "Restitution", Create);
+
+    static Sample Create(Settings settings)
     {
-        return new Restitution( settings );
+        return new Restitution(settings);
     }
 
 
-    public Restitution( Settings settings )
-        : base( settings )
+    public Restitution(Settings settings)
+        : base(settings)
     {
-        if ( settings.restart == false )
+        if (settings.restart == false)
         {
-            Draw.g_camera.m_center = { 4.0f, 17.0f };
+            Draw.g_camera.m_center = new b2Vec2(4.0f, 17.0f);
             Draw.g_camera.m_zoom = 27.5f;
         }
 
         {
             b2BodyDef bodyDef = b2DefaultBodyDef();
-            b2BodyId groundId = b2CreateBody( m_worldId, &bodyDef );
+            b2BodyId groundId = b2CreateBody(m_worldId, bodyDef);
 
             float h = 1.0f * e_count;
-            b2Segment segment = { { -h, 0.0f }, { h, 0.0f } };
+            b2Segment segment = new b2Segment(new b2Vec2(-h, 0.0f), new b2Vec2(h, 0.0f));
             b2ShapeDef shapeDef = b2DefaultShapeDef();
-            b2CreateSegmentShape( groundId, &shapeDef, &segment );
+            b2CreateSegmentShape(groundId, shapeDef, segment);
         }
 
-        for ( int i = 0; i < e_count; ++i )
+        for (int i = 0; i < e_count; ++i)
         {
             m_bodyIds[i] = b2_nullBodyId;
         }
 
-        m_shapeType = e_circleShape;
+        m_shapeType = ShapeType.e_circleShape;
 
         CreateBodies();
     }
 
     void CreateBodies()
     {
-        for ( int i = 0; i < e_count; ++i )
+        for (int i = 0; i < e_count; ++i)
         {
-            if ( B2_IS_NON_NULL( m_bodyIds[i] ) )
+            if (B2_IS_NON_NULL(m_bodyIds[i]))
             {
-                b2DestroyBody( m_bodyIds[i] );
+                b2DestroyBody(m_bodyIds[i]);
                 m_bodyIds[i] = b2_nullBodyId;
             }
         }
 
-        b2Circle circle = {};
+        b2Circle circle = new b2Circle(new b2Vec2(), 0.0f);
         circle.radius = 0.5f;
 
-        b2Polygon box = b2MakeBox( 0.5f, 0.5f );
+        b2Polygon box = b2MakeBox(0.5f, 0.5f);
 
         b2ShapeDef shapeDef = b2DefaultShapeDef();
         shapeDef.density = 1.0f;
@@ -83,24 +84,24 @@ public class Restitution : Sample
         b2BodyDef bodyDef = b2DefaultBodyDef();
         bodyDef.type = b2BodyType.b2_dynamicBody;
 
-        float dr = 1.0f / ( e_count > 1 ? e_count - 1 : 1 );
-        float x = -1.0f * ( e_count - 1 );
+        float dr = 1.0f / (e_count > 1 ? e_count - 1 : 1);
+        float x = -1.0f * (e_count - 1);
         float dx = 2.0f;
 
-        for ( int i = 0; i < e_count; ++i )
+        for (int i = 0; i < e_count; ++i)
         {
-            bodyDef.position = { x, 40.0f };
-            b2BodyId bodyId = b2CreateBody( m_worldId, &bodyDef );
+            bodyDef.position = new b2Vec2(x, 40.0f);
+            b2BodyId bodyId = b2CreateBody(m_worldId, bodyDef);
 
             m_bodyIds[i] = bodyId;
 
-            if ( m_shapeType == e_circleShape )
+            if (m_shapeType == ShapeType.e_circleShape)
             {
-                b2CreateCircleShape( bodyId, &shapeDef, &circle );
+                b2CreateCircleShape(bodyId, shapeDef, circle);
             }
             else
             {
-                b2CreatePolygonShape( bodyId, &shapeDef, &box );
+                b2CreatePolygonShape(bodyId, shapeDef, box);
             }
 
             shapeDef.restitution += dr;
@@ -110,30 +111,27 @@ public class Restitution : Sample
 
     public override void UpdateUI()
     {
+        bool open = false;
         float height = 100.0f;
-        ImGui.SetNextWindowPos( new Vector2( 10.0f, Draw.g_camera.m_height - height - 50.0f ), ImGuiCond.Once );
-        ImGui.SetNextWindowSize( new Vector2( 240.0f, height ) );
+        ImGui.SetNextWindowPos(new Vector2(10.0f, Draw.g_camera.m_height - height - 50.0f), ImGuiCond.Once);
+        ImGui.SetNextWindowSize(new Vector2(240.0f, height));
 
-        ImGui.Begin( "Restitution", nullptr, ImGuiWindowFlags.NoResize );
+        ImGui.Begin("Restitution", ref open, ImGuiWindowFlags.NoResize);
 
         bool changed = false;
-        const char* shapeTypes[] = { "Circle", "Box" };
+        string[] shapeTypes = ["Circle", "Box"];
 
-        int shapeType = int( m_shapeType );
-        changed = changed || ImGui.Combo( "Shape", &shapeType, shapeTypes, IM_ARRAYSIZE( shapeTypes ) );
-        m_shapeType = ShapeType( shapeType );
+        int shapeType = (int)m_shapeType;
+        changed = changed || ImGui.Combo("Shape", ref shapeType, shapeTypes, shapeTypes.Length);
+        m_shapeType = (ShapeType)shapeType;
 
-        changed = changed || ImGui.Button( "Reset" );
+        changed = changed || ImGui.Button("Reset");
 
-        if ( changed )
+        if (changed)
         {
             CreateBodies();
         }
 
         ImGui.End();
     }
-
-
-
 }
-
