e114175024dc66768047f6bc050d785efa54e692
ikpil <ikpil@naver.com>
2025-02-25 23:34:35 +0900

    test

--- src/Box2D.NET.Samples/shader.cs
+++ src/Box2D.NET.Samples/shader.cs
@@ -2,208 +2,202 @@
 // SPDX-License-Identifier: MIT
 
 
+using System.Diagnostics;
+
 public static class shader
 {
     
-uint CreateProgramFromFiles( string vertexPath, string fragmentPath );
-uint CreateProgramFromStrings( string vertexString, string fragmentString );
+    public static void DumpInfoGL()
+    {
+        string renderer = (string)glGetString( GL_RENDERER );
+        string vendor = (string)glGetString( GL_VENDOR );
+        string version = (string)glGetString( GL_VERSION );
+        string glslVersion = (string)glGetString( GL_SHADING_LANGUAGE_VERSION );
 
-void CheckErrorGL();
-void DumpInfoGL();
-void PrintLogGL( int object );
+        int major, minor;
+        glGetIntegerv( GL_MAJOR_VERSION, &major );
+        glGetIntegerv( GL_MINOR_VERSION, &minor );
 
+        printf( "-------------------------------------------------------------\n" );
+        printf( "GL Vendor    : %s\n", vendor );
+        printf( "GL Renderer  : %s\n", renderer );
+        printf( "GL Version   : %s\n", version );
+        printf( "GL Version   : %d.%d\n", major, minor );
+        printf( "GLSL Version : %s\n", glslVersion );
+        printf( "-------------------------------------------------------------\n" );
+    }
 
-void DumpInfoGL()
-{
-	string renderer = (string)glGetString( GL_RENDERER );
-	string vendor = (string)glGetString( GL_VENDOR );
-	string version = (string)glGetString( GL_VERSION );
-	string glslVersion = (string)glGetString( GL_SHADING_LANGUAGE_VERSION );
+    public static void CheckErrorGL()
+    {
+        GLenum errCode = glGetError();
+        if ( errCode != GL_NO_ERROR )
+        {
+            printf( "OpenGL error = %d\n", errCode );
+            Debug.Assert( false );
+        }
+    }
 
-	int major, minor;
-	glGetIntegerv( GL_MAJOR_VERSION, &major );
-	glGetIntegerv( GL_MINOR_VERSION, &minor );
+    public static void PrintLogGL( uint object )
+    {
+        GLint log_length = 0;
+        if ( glIsShader( object ) )
+        {
+            glGetShaderiv( object, GL_INFO_LOG_LENGTH, &log_length );
+        }
+        else if ( glIsProgram( object ) )
+        {
+            glGetProgramiv( object, GL_INFO_LOG_LENGTH, &log_length );
+        }
+        else
+        {
+            printf( "PrintLogGL: Not a shader or a program\n" );
+            return;
+        }
 
-	printf( "-------------------------------------------------------------\n" );
-	printf( "GL Vendor    : %s\n", vendor );
-	printf( "GL Renderer  : %s\n", renderer );
-	printf( "GL Version   : %s\n", version );
-	printf( "GL Version   : %d.%d\n", major, minor );
-	printf( "GLSL Version : %s\n", glslVersion );
-	printf( "-------------------------------------------------------------\n" );
-}
+        char* log = (char*)malloc( log_length );
 
-void CheckErrorGL()
-{
-	GLenum errCode = glGetError();
-	if ( errCode != GL_NO_ERROR )
-	{
-		printf( "OpenGL error = %d\n", errCode );
-		Debug.Assert( false );
-	}
-}
+        if ( glIsShader( object ) )
+        {
+            glGetShaderInfoLog( object, log_length, nullptr, log );
+        }
+        else if ( glIsProgram( object ) )
+        {
+            glGetProgramInfoLog( object, log_length, nullptr, log );
+        }
 
-void PrintLogGL( uint object )
-{
-	GLint log_length = 0;
-	if ( glIsShader( object ) )
-	{
-		glGetShaderiv( object, GL_INFO_LOG_LENGTH, &log_length );
-	}
-	else if ( glIsProgram( object ) )
-	{
-		glGetProgramiv( object, GL_INFO_LOG_LENGTH, &log_length );
-	}
-	else
-	{
-		printf( "PrintLogGL: Not a shader or a program\n" );
-		return;
-	}
+        printf( "PrintLogGL: %s", log );
+        free( log );
+    }
 
-	char* log = (char*)malloc( log_length );
+    public static GLuint sCreateShaderFromString( string source, GLenum type )
+    {
+        GLuint shader = glCreateShader( type );
+        string sources[] = { source };
 
-	if ( glIsShader( object ) )
-	{
-		glGetShaderInfoLog( object, log_length, nullptr, log );
-	}
-	else if ( glIsProgram( object ) )
-	{
-		glGetProgramInfoLog( object, log_length, nullptr, log );
-	}
+        glShaderSource( shader, 1, sources, nullptr );
+        glCompileShader( shader );
 
-	printf( "PrintLogGL: %s", log );
-	free( log );
-}
+        int success = GL_FALSE;
+        glGetShaderiv( shader, GL_COMPILE_STATUS, &success );
 
-static GLuint sCreateShaderFromString( string source, GLenum type )
-{
-	GLuint shader = glCreateShader( type );
-	string sources[] = { source };
+        if ( success == GL_FALSE )
+        {
+            printf( "Error compiling shader of type %d!\n", type );
+            PrintLogGL( shader );
+            glDeleteShader( shader );
+            return 0;
+        }
 
-	glShaderSource( shader, 1, sources, nullptr );
-	glCompileShader( shader );
+        return shader;
+    }
 
-	int success = GL_FALSE;
-	glGetShaderiv( shader, GL_COMPILE_STATUS, &success );
+    public static uint CreateProgramFromStrings( string vertexString, string fragmentString )
+    {
+        GLuint vertex = sCreateShaderFromString( vertexString, GL_VERTEX_SHADER );
+        if ( vertex == 0 )
+        {
+            return 0;
+        }
 
-	if ( success == GL_FALSE )
-	{
-		printf( "Error compiling shader of type %d!\n", type );
-		PrintLogGL( shader );
-		glDeleteShader( shader );
-		return 0;
-	}
+        GLuint fragment = sCreateShaderFromString( fragmentString, GL_FRAGMENT_SHADER );
+        if ( fragment == 0 )
+        {
+            return 0;
+        }
 
-	return shader;
-}
+        GLuint program = glCreateProgram();
+        glAttachShader( program, vertex );
+        glAttachShader( program, fragment );
 
-uint CreateProgramFromStrings( string vertexString, string fragmentString )
-{
-	GLuint vertex = sCreateShaderFromString( vertexString, GL_VERTEX_SHADER );
-	if ( vertex == 0 )
-	{
-		return 0;
-	}
+        glLinkProgram( program );
 
-	GLuint fragment = sCreateShaderFromString( fragmentString, GL_FRAGMENT_SHADER );
-	if ( fragment == 0 )
-	{
-		return 0;
-	}
+        int success = GL_FALSE;
+        glGetProgramiv( program, GL_LINK_STATUS, &success );
+        if ( success == GL_FALSE )
+        {
+            printf( "glLinkProgram:" );
+            PrintLogGL( program );
+            return 0;
+        }
 
-	GLuint program = glCreateProgram();
-	glAttachShader( program, vertex );
-	glAttachShader( program, fragment );
+        glDeleteShader( vertex );
+        glDeleteShader( fragment );
 
-	glLinkProgram( program );
+        return program;
+    }
 
-	int success = GL_FALSE;
-	glGetProgramiv( program, GL_LINK_STATUS, &success );
-	if ( success == GL_FALSE )
-	{
-		printf( "glLinkProgram:" );
-		PrintLogGL( program );
-		return 0;
-	}
+    public static  GLuint sCreateShaderFromFile( string filename, GLenum type )
+    {
+        FILE* file = fopen( filename, "rb" );
+        if ( file == nullptr )
+        {
+            fprintf( stderr, "Error opening %s\n", filename );
+            return 0;
+        }
 
-	glDeleteShader( vertex );
-	glDeleteShader( fragment );
+        fseek( file, 0, SEEK_END );
+        long size = ftell( file );
+        fseek( file, 0, SEEK_SET );
 
-	return program;
-}
+        char* source = static_cast<char*>( malloc( size + 1 ) );
+        size_t count = fread( source, size, 1, file );
+        B2_UNUSED( count );
+        fclose( file );
 
-static GLuint sCreateShaderFromFile( string filename, GLenum type )
-{
-	FILE* file = fopen( filename, "rb" );
-	if ( file == nullptr )
-	{
-		fprintf( stderr, "Error opening %s\n", filename );
-		return 0;
-	}
+        source[size] = 0;
 
-	fseek( file, 0, SEEK_END );
-	long size = ftell( file );
-	fseek( file, 0, SEEK_SET );
+        GLuint shader = glCreateShader( type );
+        string sources[] = { source };
 
-	char* source = static_cast<char*>( malloc( size + 1 ) );
-	size_t count = fread( source, size, 1, file );
-	B2_UNUSED( count );
-	fclose( file );
+        glShaderSource( shader, 1, sources, nullptr );
+        glCompileShader( shader );
 
-	source[size] = 0;
+        int success = GL_FALSE;
+        glGetShaderiv( shader, GL_COMPILE_STATUS, &success );
 
-	GLuint shader = glCreateShader( type );
-	string sources[] = { source };
+        if ( success == GL_FALSE )
+        {
+            fprintf( stderr, "Error compiling shader of type %d!\n", type );
+            PrintLogGL( shader );
+        }
 
-	glShaderSource( shader, 1, sources, nullptr );
-	glCompileShader( shader );
+        free( source );
+        return shader;
+    }
 
-	int success = GL_FALSE;
-	glGetShaderiv( shader, GL_COMPILE_STATUS, &success );
+    public static uint CreateProgramFromFiles( string vertexPath, string fragmentPath )
+    {
+        GLuint vertex = sCreateShaderFromFile( vertexPath, GL_VERTEX_SHADER );
+        if ( vertex == 0 )
+        {
+            return 0;
+        }
 
-	if ( success == GL_FALSE )
-	{
-		fprintf( stderr, "Error compiling shader of type %d!\n", type );
-		PrintLogGL( shader );
-	}
+        GLuint fragment = sCreateShaderFromFile( fragmentPath, GL_FRAGMENT_SHADER );
+        if ( fragment == 0 )
+        {
+            return 0;
+        }
 
-	free( source );
-	return shader;
-}
+        GLuint program = glCreateProgram();
+        glAttachShader( program, vertex );
+        glAttachShader( program, fragment );
 
-uint CreateProgramFromFiles( string vertexPath, string fragmentPath )
-{
-	GLuint vertex = sCreateShaderFromFile( vertexPath, GL_VERTEX_SHADER );
-	if ( vertex == 0 )
-	{
-		return 0;
-	}
+        glLinkProgram( program );
 
-	GLuint fragment = sCreateShaderFromFile( fragmentPath, GL_FRAGMENT_SHADER );
-	if ( fragment == 0 )
-	{
-		return 0;
-	}
+        int success = GL_FALSE;
+        glGetProgramiv( program, GL_LINK_STATUS, &success );
+        if ( success == GL_FALSE )
+        {
+            printf( "glLinkProgram:" );
+            PrintLogGL( program );
+            return 0;
+        }
 
-	GLuint program = glCreateProgram();
-	glAttachShader( program, vertex );
-	glAttachShader( program, fragment );
+        glDeleteShader( vertex );
+        glDeleteShader( fragment );
 
-	glLinkProgram( program );
-
-	int success = GL_FALSE;
-	glGetProgramiv( program, GL_LINK_STATUS, &success );
-	if ( success == GL_FALSE )
-	{
-		printf( "glLinkProgram:" );
-		PrintLogGL( program );
-		return 0;
-	}
-
-	glDeleteShader( vertex );
-	glDeleteShader( fragment );
-
-	return program;
-}
+        return program;
+    }
 
 }
