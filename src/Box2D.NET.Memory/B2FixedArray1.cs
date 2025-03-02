35d87b1aa4ebb856d485a119d7a4cf6177c1b987
ikpil <ikpil@naver.com>
2025-03-02 11:53:06 +0900

    add B2FixedArray1

---
+++ src/Box2D.NET.Memory/B2FixedArray1.cs
@@ -0,0 +1,38 @@
+﻿// SPDX-FileCopyrightText: 2025 Ikpil Choi(ikpil@naver.com)
+// SPDX-License-Identifier: MIT
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Box2D.NET.Core
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct B2FixedArray1<T> where T : unmanaged
+    {
+        public const int Length = 1;
+
+        public T v0000;
+
+        public ref T this[int index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => ref GetElement(index);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private unsafe ref T GetElement(int index)
+        {
+            if (0 > index || Length <= index)
+                throw new IndexOutOfRangeException();
+
+            return ref Unsafe.AsRef<T>(Unsafe.Add<T>(Unsafe.AsPointer(ref v0000), index));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe Span<T> AsSpan()
+        {
+            return new Span<T>(Unsafe.AsPointer(ref v0000), Length);
+        }
+    }
+}
