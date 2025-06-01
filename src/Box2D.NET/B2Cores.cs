8cd095ac1be81ccdfbedde0b418c4719a3276907
ikpil <ikpil@naver.com>
2025-06-01 13:01:33 +0900

    add AggressiveInlining in B2Cores

--- src/Box2D.NET/B2Cores.cs
+++ src/Box2D.NET/B2Cores.cs
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2025 Ikpil Choi(ikpil@naver.com)
 // SPDX-License-Identifier: MIT
 
+using System.Runtime.CompilerServices;
 using static Box2D.NET.B2MathFunction;
 using static Box2D.NET.B2Diagnostics;
 using static Box2D.NET.B2Constants;
@@ -36,6 +37,7 @@ namespace Box2D.NET
         /// However, you are now on the hook for coming up with good values for gravity, density, and
         /// forces.
         /// @warning This must be modified before any calls to Box2D
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void b2SetLengthUnitsPerMeter(float lengthUnits)
         {
             B2_ASSERT(b2IsValidFloat(lengthUnits) && lengthUnits > 0.0f);
@@ -43,66 +45,79 @@ namespace Box2D.NET
         }
 
         /// Get the current length units per meter.
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float b2GetLengthUnitsPerMeter()
         {
             return b2_lengthUnitsPerMeter;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void B2_CHECK_DEF(ref B2WheelJointDef def)
         {
             B2_ASSERT(def.internalValue == B2_SECRET_COOKIE);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void B2_CHECK_DEF(ref B2WeldJointDef def)
         {
             B2_ASSERT(def.internalValue == B2_SECRET_COOKIE);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void B2_CHECK_DEF(ref B2PrismaticJointDef def)
         {
             B2_ASSERT(def.internalValue == B2_SECRET_COOKIE);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void B2_CHECK_DEF(ref B2RevoluteJointDef def)
         {
             B2_ASSERT(def.internalValue == B2_SECRET_COOKIE);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void B2_CHECK_DEF(ref b2FilterJointDef def)
         {
             B2_ASSERT(def.internalValue == B2_SECRET_COOKIE);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void B2_CHECK_DEF(ref B2MouseJointDef def)
         {
             B2_ASSERT(def.internalValue == B2_SECRET_COOKIE);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void B2_CHECK_DEF(ref B2MotorJointDef def)
         {
             B2_ASSERT(def.internalValue == B2_SECRET_COOKIE);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void B2_CHECK_DEF(ref B2DistanceJointDef def)
         {
             B2_ASSERT(def.internalValue == B2_SECRET_COOKIE);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void B2_CHECK_DEF(ref B2ChainDef def)
         {
             B2_ASSERT(def.internalValue == B2_SECRET_COOKIE);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void B2_CHECK_DEF(ref B2BodyDef def)
         {
             B2_ASSERT(def.internalValue == B2_SECRET_COOKIE);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void B2_CHECK_DEF(ref B2WorldDef def)
         {
             B2_ASSERT(def.internalValue == B2_SECRET_COOKIE);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void B2_CHECK_DEF(ref B2ShapeDef def)
         {
             B2_ASSERT(def.internalValue == B2_SECRET_COOKIE);
