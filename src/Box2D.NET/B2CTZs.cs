2059efd9d6aa19d6fc05700dd02a0a00dcc781d4
ikpil <ikpil0@gmail.com>
2025-07-12 18:01:04 +0900

    refactor: B2CTZTest

--- src/Box2D.NET/B2CTZs.cs
+++ src/Box2D.NET/B2CTZs.cs
@@ -2,7 +2,6 @@
 // SPDX-FileCopyrightText: 2025 Ikpil Choi(ikpil@naver.com)
 // SPDX-License-Identifier: MIT
 
-using System.Numerics;
 using System.Runtime.CompilerServices;
 
 namespace Box2D.NET
--- test/Box2D.NET.Test/B2CTZTest.cs
+++ test/Box2D.NET.Test/B2CTZTest.cs
@@ -10,40 +10,42 @@ namespace Box2D.NET.Test;
 
 public class B2CTZTest
 {
-    // uint에 대해 trailing zero count (CTZ)
-    public static uint CTZ32(uint block)
-    {
-        return (uint)BitOperations.TrailingZeroCount(block);
-    }
-
-    // uint에 대해 leading zero count (CLZ)
-    public static uint CLZ32(uint value)
-    {
-        return (uint)BitOperations.LeadingZeroCount(value);
-    }
-
-    // ulong에 대해 trailing zero count (CTZ)
-    public static uint CTZ64(ulong block)
-    {
-        return (uint)BitOperations.TrailingZeroCount(block);
-    }
-
     [Test]
     public void Test()
     {
         // 추가적인 엣지 케이스 테스트
-        uint[] testCases32 = { 0, 1, 2, 4, 8, 16, 31, 32, 64, 128, 255, 256, 1023, 1024, 4294967295, uint.MaxValue };
-        ulong[] testCases64 = { 0, 1, 2, 4, 8, 16, 31, 32, 64, 128, 255, 256, 1023, 1024, 18446744073709551615, ulong.MaxValue };
+        uint[] testCases32 =
+        {
+            0, 1, 2, 4, 
+            8, 16, 31, 32, 
+            64, 128, 255, 256, 
+            1023, 1024, 4294967295, uint.MaxValue,
+            0x0000FFFF, 0xFFFF0000, 0x0F0F0F0F, 0xF0F0F0F0,
+            0xAAAAAAAA, 0x55555555, 0x80000001, 0x7FFFFFFF,
+            0x0000F000, 0x00FF00FF, 0xC0000000, 0x00000003, 0xFFFFFFFF
+        };
+        ulong[] testCases64 =
+        {
+            0, 1, 2, 4, 
+            8, 16, 31, 32, 
+            64, 128, 255, 256, 
+            1023, 1024, 18446744073709551615, ulong.MaxValue,
+            0x00000000FFFFFFFFul, 0xFFFFFFFF00000000ul, 0xAAAAAAAAAAAAAAAAul, 0x8000000000000001ul,
+            0x7FFFFFFFFFFFFFFFul, 0x000F000000000000ul, 0xF0F0F0F0F0F0F0F0ul, 0x000000000000FFFFul,
+            0x00FF00FF00FF00FFul
+        };
 
         foreach (uint value in testCases32)
         {
-            Assert.That(b2CTZ32(value), Is.EqualTo(CTZ32(value)), $"CTZ32 failed for {value}");
-            Assert.That(b2CLZ32(value), Is.EqualTo(CLZ32(value)), $"CLZ32 failed for {value}");
+            Assert.That(b2CTZ32(value), Is.EqualTo(BitOperations.TrailingZeroCount(value)), $"CTZ32 failed for {value}");
+            Assert.That(b2CLZ32(value), Is.EqualTo(BitOperations.LeadingZeroCount(value)), $"CLZ32 failed for {value}");
+            Assert.That(b2PopCount64(value), Is.EqualTo(BitOperations.PopCount(value)), $"PopCount64 failed for {value}");
         }
 
         foreach (ulong value in testCases64)
         {
-            Assert.That(b2CTZ64(value), Is.EqualTo(CTZ64(value)), $"CTZ64 failed for {value}");
+            Assert.That(b2CTZ64(value), Is.EqualTo(BitOperations.TrailingZeroCount(value)), $"CTZ64 failed for {value}");
+            Assert.That(b2PopCount64(value), Is.EqualTo(BitOperations.PopCount(value)), $"PopCount64 failed for {value}");
         }
     }
 }
