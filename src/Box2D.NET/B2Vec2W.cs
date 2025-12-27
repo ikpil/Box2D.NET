d816f03c0c5649e8e1960022e276b6292ae282ad
ikpil <ikpil0@gmail.com>
2025-12-27 12:51:32 +0900

    refactor: Optimize B2FloatW and B2Vec2W operations with 'in' modifier
    - Updated `B2ContactSolvers.cs` math functions (`b2AddW`, `b2SubW`, `b2MulW`, etc.) to pass `B2FloatW` and `B2Vec2W` parameters by reference using the `in` keyword.
    - Updated `B2Vec2W` constructor to accept `B2FloatW` parameters by reference.
    - These changes reduce struct copying overhead and improve performance for SIMD-related operations.

--- src/Box2D.NET/B2Vec2W.cs
+++ src/Box2D.NET/B2Vec2W.cs
@@ -10,7 +10,7 @@ namespace Box2D.NET
         public B2FloatW X;
         public B2FloatW Y;
 
-        public B2Vec2W(B2FloatW X, B2FloatW Y)
+        public B2Vec2W(in B2FloatW X, in B2FloatW Y)
         {
             this.X = X;
             this.Y = Y;
